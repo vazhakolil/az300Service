f9a42430a99855050eabb8797564267bc127aaa4
vazhakolil <vazhakolil.r@gmail.com>
2019-04-29 21:05:17 -0400

    dev

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AZ300WebApp-NodeWebapp######SlotTest11' });
+  res.render('index', { title: 'AZ300WebApp-NodeWebapp######SlotTest1123' });
 });
 
 module.exports = router;
